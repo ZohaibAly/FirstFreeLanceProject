352b7dd9071f0860c7a3c3cb2006c363c5b117cc
Zohaib <mzohaibali223@gmail.com>
2025-09-13 23:11:13 +0500

    make theme.js and little bit fix react

---
+++ constants/theme.js
@@ -0,0 +1,15 @@
+// src/constants/theme.js
+const theme = {
+  colors: {
+    blue: "#0D2844",
+    orange: "#F79321",
+    gray: "#444545",
+  },
+  fonts: {
+    heading: '"Montserrat", sans-serif',
+    body: '"Poppins", sans-serif',
+  },
+};
+
+export default theme;
+export { theme };
